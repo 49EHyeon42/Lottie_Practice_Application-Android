899ccf5f6f7402b060c6fad76b3d91961a8eccd2
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-10-09 20:40:48 +0900

    init: init project

---
+++ app/src/main/java/dev/ehyeon/lottiepracticeapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.ehyeon.lottiepracticeapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
