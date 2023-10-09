865ce0ae63d69a5855380b69c2239ffa03557627
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-10-09 21:57:34 +0900

    feat: 버튼을 누르면 lottie 3초 적용

--- app/src/main/java/dev/ehyeon/lottiepracticeapplication/MainActivity.java
+++ app/src/main/java/dev/ehyeon/lottiepracticeapplication/MainActivity.java
@@ -1,8 +1,13 @@
 package dev.ehyeon.lottiepracticeapplication;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        LottieAnimationView lottieAnimationView = findViewById(R.id.lottie_animation_view);
+
+        button.setOnClickListener(ignored -> {
+            lottieAnimationView.setVisibility(View.VISIBLE);
+            lottieAnimationView.playAnimation();
+
+            // something async or await
+            new Handler().postDelayed(() -> {
+                lottieAnimationView.setVisibility(View.GONE);
+                lottieAnimationView.pauseAnimation();
+            }, 3000);
+        });
     }
 }
